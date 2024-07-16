c220aba38be7e1eb6d5a4025d3544f74fc171a29
Ivan Horidko <yellowthunder873@gmail.com>
2024-07-16 14:47:40 +0500

    Update AddItemForm.test.js
    Finish AddItemForm component testing

--- src/Entities/AddItemForm/components/AddItemForm.test.js
+++ src/Entities/AddItemForm/components/AddItemForm.test.js
@@ -1,13 +1,14 @@
 import {createRoot} from "react-dom/client";
-import {fireEvent, screen, waitFor} from "@testing-library/react";
+import {screen, waitFor} from "@testing-library/react";
 import React from "react";
 import {AddItemForm} from "./AddItemForm";
 import {act} from "react"
 import userEvent from '@testing-library/user-event';
-import {debug} from "node:util";
+
+const addItemMock = jest.fn(); // Mock function
 
 it('should show text content as Buy a coffee', async () => {
-    // arrange
+    // Arrange
     const container = document.createElement('div');
     document.body.appendChild(container);
     act(() => {
@@ -22,14 +23,45 @@ it('should show text content as Buy a coffee', async () => {
     const textField = formContainer.querySelector('input');
     expect(textField).toBeInTheDocument();
 
+    // Act
+    await userEvent.type(textField, 'Buy a coffee');
+
+    // Assert
+    await waitFor(() => expect(textField).toHaveValue('Buy a coffee'));
+
+    // Manually cleanup
+    document.body.removeChild(container);
+});
+
+it('should clear the input field after pressing the button', async () => {
+    // Arrange
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    act(() => {
+        const root = createRoot(container);
+        root.render(<AddItemForm addItem={addItemMock} />); // Pass the mock function as a prop
+    });
+
+    const formContainer = await screen.findByTestId('add-item-form');
+    expect(formContainer).toBeInTheDocument();
+
+    const textField = formContainer.querySelector('input');
+    expect(textField).toBeInTheDocument();
+
     const button = await screen.findByTestId('add-item-button');
     expect(button).toBeInTheDocument();
 
     // Act
-    await userEvent.type(textField, 'Buy a coffee');
 
-    // Wait for the input value to be updated
+    await userEvent.type(textField, 'Buy a coffee');
     await waitFor(() => expect(textField).toHaveValue('Buy a coffee'));
+
+    userEvent.click(button);
+
+    // Arrange
+    await waitFor(() => {
+        expect(textField).toHaveValue('');
+    });
 
     // Manually cleanup
     document.body.removeChild(container);
